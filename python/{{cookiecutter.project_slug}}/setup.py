0cf1365767a5d1bc9e6d340c9a1524ecdfdc1a53
Boni Lindsley <boni@lindsley.xyz>
2021-10-27 15:56:00 +0100

    refactor[python]: setup from script to config

--- python/{{cookiecutter.project_slug}}/setup.py
+++ python/{{cookiecutter.project_slug}}/setup.py
@@ -1,37 +1,10 @@
 #!/usr/bin/env python3
 
+# This script is required for an editable install.
+# Editable installs are needed for tox.
+# https://setuptools.pypa.io/en/latest/setuptools.html#setup-cfg-only-projects
+
 # Standard libraries.
 import setuptools  # type: ignore
 
-setuptools.setup(
-    name="{{cookiecutter.project_name}}",
-    version="{{cookiecutter.version}}",
-    description="{{cookiecutter.project_short_description}}",
-    author="{{cookiecutter.full_name}}",
-    author_email="{{cookiecutter.email}}",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    license="MIT",
-    install_requires=[
-        "click",
-    ],
-    entry_points={
-        "console_scripts": [
-            "{{cookiecutter.project_name}} = {{cookiecutter.project_slug}}.__main__:main",
-        ],
-    },
-    extras_require={
-        "dev": [
-            "black",
-            "coverage[toml]",
-            "mypy",
-            "pytest",
-            "recommonmark",
-            "sphinx",
-            "tox",
-            "types-pkg_resources",
-            "types-six",
-        ],
-    },
-    python_requires=">= 3.9",
-)
+setuptools.setup()
